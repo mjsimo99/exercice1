bf720c33b47868bc1997372514b642109101dbb2
mjsimo99 <mohashark21@gmail.com>
2023-09-16 07:01:49 -0700

    add method for client

--- src/interfeces/Iclient.java
+++ src/interfeces/Iclient.java
@@ -3,7 +3,6 @@ import dto.Client;
 
 public interface Iclient {
 
-    Project Add(Project project);
-    Project Update(Project project);
-    Boolean Delete(String code);
+    Client Add(Client client);
+    Client Update(Client client);
 }
